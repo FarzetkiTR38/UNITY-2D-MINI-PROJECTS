a652d82cb46fd72a971f1a2166ce752bca20bf95
FarzetkiTR38 <106279672+FarzetkiTR38@users.noreply.github.com>
2025-11-20 23:34:34 +0300

    Deleted test projects

--- FVampireSurvivors/Assets/Scripts/EnemySpawner.cs
+++ FVampireSurvivors/Assets/Scripts/EnemySpawner.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public GameObject enemyToSpawn;
+
+    public float timeToSpawn;
+    private float spawnCounter;
+
+
     void Start()
     {
-        
+        spawnCounter = timeToSpawn;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnCounter -= Time.deltaTime;
+        if(spawnCounter <= 0)
+        {
+            spawnCounter = timeToSpawn;
+
+            Instantiate(enemyToSpawn, transform.position, transform.rotation);
+        }
     }
 }
